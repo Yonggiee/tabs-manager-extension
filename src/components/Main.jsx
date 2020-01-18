5e938d0cbe4d8f32141351ed23358f722d342749
winphong <winphong@comp.nus.edu.sg>
2020-01-18 17:33:05 +0800

    allow chrome to work on main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,50 @@
+/*global chrome*/
 import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import TextField from "@material-ui/core/TextField";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 class Main extends Component {
-  state = {};
+  state = {
+    tabs: []
+  };
+
+  printChromeTabs = () => {
+    let tabOpened = [];
+    chrome.tabs.query({}, function(tabs) {
+      tabOpened = tabs;
+      console.log(tabs);
+    });
+    this.setState(tabOpened);
+  };
 
   render() {
     return (
-      <Grid container xs={12}>
-        <Grid item xs={12}>
+      <Grid
+        container
+        xs={12}
+        style={{ padding: "5%", backgroundColor: "black" }}
+      >
+        <Grid item xs={12} style={{ padding: "2%", backgroundColor: "orange" }}>
           <Card>
-            <h1> Hello </h1>
+            <TextField id="standard-basic" fullWidth />
+            <Card>
+              <img src="./logo192.png"></img>
+              <img src="./logo192.png"></img>
+              <img src="./logo192.png"></img>
+              <img src="./logo192.png"></img>
+            </Card>
           </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Fab color="primary" aria-label="add">
+              <AddIcon onClick={this.printChromeTabs} />
+            </Fab>
+          </div>
         </Grid>
       </Grid>
     );
